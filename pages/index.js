2286d4bb5e1e4233163fc1c2f7f5f2fcc6e7f071
Adel <htwr86@gmail.com>
2021-04-18 17:46:54 +0300

    jit mode on

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -8,6 +9,19 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <header className='p-4 fixed top-0 bg-gray-300 w-full'>
+        <navbar>
+          <Link href='/' className='m-4'>
+            Home
+          </Link>
+          <Link href='/users' className='mx-4'>
+            Users
+          </Link>
+          <Link href='/about' className='mx-4'>
+            About
+          </Link>
+        </navbar>
+      </header>
       <div>
         <h1 className=' text-6xl'>Home</h1>
       </div>
